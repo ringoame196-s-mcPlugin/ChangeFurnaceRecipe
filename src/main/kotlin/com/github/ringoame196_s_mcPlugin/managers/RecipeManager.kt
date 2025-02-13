4587259dda2b3a95dcd5d8a424aa4ed2c6d27f1a
りんご飴196 <kazumac41@gmail.com>
2025-02-13 21:26:55 +0900

    ✨  :sparkles: keyも管理できるように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/managers/RecipeManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/managers/RecipeManager.kt
@@ -13,21 +13,32 @@ object RecipeManager {
         while (recipes.hasNext()) {
             val recipe = recipes.next()
             if (recipe is FurnaceRecipe) {
-                Data.furnaceRecipes[recipe.input.type] = recipe.result.type
+                val recipeData = RecipeData(recipe.key, recipe.result.type)
+                Data.furnaceRecipes[recipe.input.type] = recipeData
             }
         }
     }
 
     fun acquisitionRecipe(material: Material): Material {
-        return Data.furnaceRecipes[material] ?: Material.AIR
+        return Data.furnaceRecipes[material]?.resultType ?: Material.AIR
+    }
+
+    fun acquisitionKey(material: Material): NamespacedKey? {
+        return Data.furnaceRecipes[material]?.key
     }
 
     fun saveRecipe(inputItem: ItemStack, resultItem: ItemStack) {
+        val oldKey = acquisitionKey(inputItem.type)
+
+        // 古いレシピデータを削除する
+        if (oldKey != null) Bukkit.removeRecipe(oldKey)
+
         val key = NamespacedKey(Data.PLUGIN_NAME.lowercase(), inputItem.type.toString().lowercase()) // レシピのキー（ユニーク識別用）
 
         if (Bukkit.getRecipe(key) != null) Bukkit.removeRecipe(key) // 重複阻止のため 削除する
         val furnaceRecipe = FurnaceRecipe(key, resultItem, inputItem.type, 0.1f, 200)
         Bukkit.addRecipe(furnaceRecipe) // サーバーにレシピを登録
-        Data.furnaceRecipes[inputItem.type] = resultItem.type // データを更新
+        val recipeData = RecipeData(key, resultItem.type)
+        Data.furnaceRecipes[inputItem.type] = recipeData // データを更新
     }
 }
