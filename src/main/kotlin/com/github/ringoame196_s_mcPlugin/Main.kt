a87327d60e4e9dbe61ec2dfd1faa6b9bd61c69bf
りんご飴196 <kazumac41@gmail.com>
2025-02-15 14:36:31 +0900

    ✨  :sparkles: yamlファイル追加

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -2,16 +2,22 @@ package com.github.ringoame196_s_mcPlugin
 
 import com.github.ringoame196_s_mcPlugin.commands.Command
 import com.github.ringoame196_s_mcPlugin.commands.CommandConst
+import com.github.ringoame196_s_mcPlugin.data.Data
 import com.github.ringoame196_s_mcPlugin.events.RecipeGUIEvent
 import com.github.ringoame196_s_mcPlugin.managers.RecipeManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     private val plugin = this
-    private val recipeManager = RecipeManager()
     override fun onEnable() {
         super.onEnable()
-        recipeManager.setRecipe()
+        if (!plugin.dataFolder.exists()) plugin.dataFolder.mkdirs()
+        saveResource("recipe.yml", false)
+        Data.pluginDataFolderPath = plugin.dataFolder.path
+
+        val recipeManager = RecipeManager()
+        recipeManager.loadRecipe()
+        recipeManager.setRecipeMap()
         server.pluginManager.registerEvents(RecipeGUIEvent(), plugin)
         val command = getCommand(CommandConst.COMMAND_NAME)
         command!!.setExecutor(Command())
